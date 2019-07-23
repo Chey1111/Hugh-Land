4ff2f0bfbfab44a19c5d1b1f956845c540127d58
Chey <53188215+Chey1111@users.noreply.github.com>
2019-07-22 22:44:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 game = 0
+location = "porch"
+HP = 100
+MO = 0
+
 while game < 1:
-  room = "Porch"
-  print("You are at {}.".format(room))
-  action =  input("WHAT NEXT?:")
+  print("HP:{}".format(HP))
+  print("You are at {}.".format(location))
+
+  if location == "porch":
+    print("To the north is the kitchen.")
+    
+  if location == "kitchen":
+    print("To the south is the porch.")
+  
+  next =  input("WHAT NEXT?:")
+  # print(" {}").format(next))
+  if next == "go kitchen" and location == "porch":
+      location = "kitchen"
+  if next == "go porch" and location == "kitchen":
+      location = "porch"
