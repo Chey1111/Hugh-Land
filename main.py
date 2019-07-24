4209f50e84015621de2ee2c1e2430fe488250f0d
Chey <53188215+Chey1111@users.noreply.github.com>
2019-07-24 02:14:28 -0500

    Update main.py
    it's working again

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
-##################################################################
-# Title":    Hugh-Land                                           #
-# By:        Steven Jacks and Jeff Shipton                       #
-# Date:      Started 7/23/2019                                   #
-# Objective: Wtf?  go bleep urself.  This a text based game niku #
-##################################################################
+# ##################################################################
+# # Title":    Hugh-Land                                           #
+# # By:        Steven Jacks and Jeff Shipton                       #
+# # Date:      Started 7/23/2019                                   #
+# # Objective: Wtf?  go bleep urself.  This a text based game niku #
+# ##################################################################
 
 #MAP KEY:
 
-##########   ###########   ###########
-# CLOSET #---# KITCHEN #---# BEDROOM #
-##########   ###########   ###########
-                  |
-               #########
-               # PORCH #
-               #########
-                  |
-###########   ############   #######   #########
-# LIBRARY #---# SIDEWALK #---# BAR #---# STORE #
-###########   ############   #######   #########
-      |            |
-############   ##########
-# COMPUTER #   # STREET #
-############   ##########
-                   |
-            ################
-            # STOCK MARKET #
-            ################
-                   |
- ############   ########
- # DUMPSTER #---# ALLEY#
- ############   ########
+# ##########   ###########   ###########
+# # CLOSET #---# KITCHEN #---# BEDROOM #
+# ##########   ###########   ###########
+#                  |
+#                #########
+#                # PORCH #
+#                #########
+#                   |
+# ###########   ############   #######   #########
+# # LIBRARY #---# SIDEWALK #---# BAR #---# STORE #
+# ###########   ############   #######   #########
+#       |            |
+# ############   ##########
+# # COMPUTER #   # STREET #
+# ############   ##########
+#                    |
+#             ################
+#             # STOCK MARKET #
+#             ################
+#                    |
+#  ############   ########
+#  # DUMPSTER #---# ALLEY#
+#  ############   ########
 
 # This is where we set up the initial game variables
  # when start show game is not over
-   game = 0
+game = 0
 # This variable shows where the starting location is at.
 # You can change the hardcode if you wish for a different starting location.
-    location = "porch"
+location = "porch"
 # HP is made for what call hitpoints.  If your
 # hitpoints drop below zero then you are diededs.
-  HP = 100
+HP = 100
 # This variable is made for tracking the players amount
 # of money carried.  Default is that it starts at zero.
-  MO = 0
+MO = 0
 
 # THIS IS WHERE THE TEXT-GAME LOOP BEGINS
 
@@ -53,7 +53,7 @@ while game < 1:
 # Upon loop of text interface show basic variable values
 # As associated with the players view of the game.
 
-print("HP:{}".format(HP))
+  print("HP:{}".format(HP))
   print("You are at {}.".format(location))
 
 ###########################################################
@@ -124,11 +124,7 @@ print("HP:{}".format(HP))
 #  \
 # #####
 
-print(next)
   next =  input("WHAT NEXT?:")
-
-# This line shows how to display the users input back
-# to them for the fucking hell of it.
 
 # #####
 #  /
@@ -136,12 +132,13 @@ print(next)
 #  \
 # #####
 
-print(next)
+# This line shows how to display the users input back
+# to them for the fucking hell of it.
 
-if location == "porch" and next == "go kitchen":
+  print(next)
+
+  if location == "closet" and next == "go kitchen":
     location = "kitchen"
-  if location == "porch" and next == "go sidewalk":
-    location = "sidewalk"
 
   if location == "kitchen" and next == "go porch":
     location = "porch"
@@ -150,15 +147,62 @@ if location == "porch" and next == "go kitchen":
   if location == "kitchen" and next == "go bedroom":
     location = "bedroom"
 
-  if location == "closet" and next == "go kitchen":
-    location = "kitchen"
-    
   if location == "bedroom" and next == "go kitchen":
     location = "kitchen"
-    
+
+  if location == "porch" and next == "go kitchen":
+    location = "kitchen"
+  if location == "porch" and next == "go sidewalk":
+    location = "sidewalk"
+
+  if location == "library" and next == "go sidewalk":
+    locatoin = "sidewalk"
+
+  if location == "sidewalk" and next == "go porch":
+    location = "porch"
+  if location == "sidewalk" and next == "go street":
+    location = "street"
+  if location == "sidewalk" and next == "go library":
+    location = "library"
+  if location =="sidewalk" and next == "go bar":
+    location = "bar"
+
+  if location == "bar" and next == "go sidewalk":
+    location = "sidewalk"
+  if location == "bar" and next == "go store":
+    location = "store"
+
+  if location == "store" and next == "go bar":
+    location = "bar"
+
+  if location == "computer" and next == "go library":
+    location = "library"
+
+  if location == "street" and next == "go sidewalk":
+    location = "sidewalk"
+  if location == "street" and next == "go stock market":
+    locatoin = "stock market"
+
+  if location == "stock market" and next == "go street":
+    location = "street"
+  if location == "stock market" and next == "go alley":
+    location = "alley"
+
+  if location == "dumpster" and next == "go alley":
+    location = "alley"
+
+  if location == "alley" and next == "go stock market":
+    location = "stock market"
+  if location == "alley" and next == "go dumpster":
+    location = "dumpster"
+	
 #####################  
 # THIS IS FOR WARPS #
 #####################
 
-  if next == "warp porch":
-    location = "porch"
+if next == "warp porch":
+  location = "porch"
+
+if next == "warp secret room":
+  location = "secret room"
+
