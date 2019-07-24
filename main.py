a01a1af54b160270aa901b24f4ccf674aff42284
Chey <53188215+Chey1111@users.noreply.github.com>
2019-07-24 00:43:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,72 @@ while game < 1:
 
   if location == "porch":
     print("To the north is the kitchen.")
+    print("To the south is the sidewalk.")
     
   if location == "kitchen":
     print("To the south is the porch.")
+    print("To the east is the closet.")
+    print("To the west is the bedroom.")
+    
+  if location == "closet":
+    print("To the east is the kitchen.")
+    
+  if location == "bedroom":
+    print("To the west is the kitchen.")
+    
+  if location == "sidewalk":
+    print("To then north is the porch.")
+    print("To the south is the street.")
+    print("To the east is the library.")
+    print("To the west is the bar.")
+    
+  if location == "library":
+    print("To the south is the computer.")
+    print("To the east is the sidewalk.")
+    
+  if location == "computer":
+    print("To the north is the library.")
+    
+  if location == "bar":
+    print("To the wast is the store.")
+    print("To the east is the sidewalk.")
+    
+  if location == "store":
+    print("To the west is the bar.")
+    
+  if location == "street":
+    print("To the north is the sidewalk.")
+    print("To the south is the stock market.")
+    
+  if location == "stock market":
+    print("To the north is the street.")
+    print("To the south is the alley.")
+    
+  if location == "alley":
+    print("To the north is the stock market.")
+    print("To the west is the dumpster.")
+    
+  if location == "dumpster":
+    print("To the east is the alley.")
   
   next =  input("WHAT NEXT?:")
   print(next)
-  if next == "go kitchen" and location == "porch":
-      location = "kitchen"
-  if next == "go porch" and location == "kitchen":
-      location = "porch"
+  if location == "porch" and next == "go kitchen":
+    location = "kitchen"
+  if location == "porch" and next == "go sidewalk":
+    location = "sidewalk"
+
+  if location == "kitchen" and next == "go porch":
+    location = "porch"
+  if location == "kitchen" and next == "go closet":
+    location = "closet"
+  if location == "kitchen" and next == "go bedroom":
+    location = "bedroom"
+
+  if location == "closet" and next == "go kitchen":
+    location = "kitchen"
+    
+  if location == "bedroom" and next == "go kitchen":
+    location = "kitchen"
+    
+  
